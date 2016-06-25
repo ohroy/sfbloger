0cfcfbf9b5351fe147b0e852636d6d6d88e12463
rozbo <75200306@qq.com>
2016-06-25 18:59:35 +0800

    init

---
+++ post.js
@@ -0,0 +1,33 @@
+'use strict';
+var fs = require('fs');
+var front = require('hexo-front-matter');
+var config = require('./config.json')
+
+function _getFileStr() {
+    var path = process.argv.splice(2)[0];
+    if (typeof path !== 'string') throw new TypeError('post path error');
+    path = config.evn_path + path;
+    //console.log(path);
+    if (!fs.existsSync(path)) throw new Error('post not found');
+    var post_str = fs.readFileSync(path, 'utf-8');
+    return post_str;
+}
+
+function _parsePost(post_str) {
+    var objPost = front.parse(post_str);
+    return {
+        title: objPost.title,
+        //过滤所有html标签
+        content: objPost._content.replace(/<[^>]+>/g, ""),
+        tag: objPost.tag //category
+    }
+}
+
+function post() {
+    var post_str = _getFileStr();
+    var objPost = _parsePost(post_str);
+    return objPost;
+}
+
+
+module.exports = post;
---
+++ tags.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var obj_tag = require('./tag.json')
+
+
+
+function tag(str) {
+    var ret = ''; //windows
+    if (typeof str !== 'string') throw new TypeError('str must be a string!');
+    for (var p in obj_tag) {
+
+        var tag_type = obj_tag[p];
+        for (var tag_i in tag_type) {
+            if (tag_type[tag_i].name == str) {
+                ret = tag_type[tag_i].id;
+                break;
+            }
+        }
+    }
+    return ret;
+}
+
+function tags(strs) {
+    var tagIds = ['1040000000089709'];
+    for (var i in strs) {
+        var tmpStr = tag(strs[i]);
+        if (tmpStr = 'nodejs') {
+            tmpStr = 'node.js';
+        }
+        var tmpId = tmpStr;
+        if (tagIds.indexOf(tmpId) == -1 && tmpId != '') {
+            tagIds.push(tmpId);
+        }
+    }
+    return tagIds;
+}
+
+module.exports = tags;
